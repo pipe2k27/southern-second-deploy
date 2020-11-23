f2327d3f93239d2d7390582bfba1b5b75166328c
felipe <feliperarnau@gmail.com>
2020-11-23 19:15:08 -0300

    Updates

---
+++ precache-manifest.1dfad0df227fdac7d24b10ee85e909b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3040d1ca8732b36a9f1c9a961f379f98",
+    "url": "/southern-second-deploy/index.html"
+  },
+  {
+    "revision": "ffacb051ccd07dcc4ee4",
+    "url": "/southern-second-deploy/static/css/main.efe57ef9.chunk.css"
+  },
+  {
+    "revision": "fb2de9c161ca72998dd3",
+    "url": "/southern-second-deploy/static/js/2.09652de2.chunk.js"
+  },
+  {
+    "revision": "7d756366b06d2ed63185357d9a35e8e9",
+    "url": "/southern-second-deploy/static/js/2.09652de2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffacb051ccd07dcc4ee4",
+    "url": "/southern-second-deploy/static/js/main.ef7116e1.chunk.js"
+  },
+  {
+    "revision": "e4eb19c84756ac9ef986",
+    "url": "/southern-second-deploy/static/js/runtime-main.f1183a0d.js"
+  }
+]);
